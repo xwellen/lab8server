bb2e0f414167384e90ae3f3399af89015be2fb7e
Vsevolod <vsevolod628@gmail.com>
2020-06-04 16:10:28 +0300

    строит таблицы в бд

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import ServerSocket.Controller;
 //import Utils.JSON.ParserJson;
-
+import Utils.Database.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Runtime.getRuntime().addShutdownHook(new Thread(ParserJson::collectionToJson));
-        Controller controller = new Controller();
-        controller.run(args[0]);
+////        Runtime.getRuntime().addShutdownHook(new Thread(ParserJson::collectionToJson));
+//        Controller controller = new Controller();
+//        controller.run(args[0]);
+        try {
+            DatabaseManagerImpl databaseManager = new DatabaseManagerImpl();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Utils/Database/DatabaseManagerImpl.java
+++ src/Utils/Database/DatabaseManagerImpl.java
@@ -1,4 +1,41 @@
 package Utils.Database;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
 public class DatabaseManagerImpl {
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String user = "postgres";
+    private final String password = "...";
+    private Connection connection;
+    private Statement statement;
+    public DatabaseManagerImpl() throws SQLException {
+        connection = DriverManager.getConnection(url, user, password);
+        statement = connection.createStatement();
+
+        buildTables();
+    }
+
+    private void buildTables(){
+        ScriptRunner sr = new ScriptRunner(connection);
+        try {
+            Files.walk(Paths.get("src/Utils/Database/Sql"))
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
+                        Reader reader = null;
+                        try {
+                            reader = new BufferedReader(new FileReader(String.valueOf(path)));
+                            sr.runScript(reader);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
