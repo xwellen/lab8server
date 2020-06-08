27322a1f3ffc79d5caf5ef5ad9fee7433192881b
Vsevolod <vsevolod628@gmail.com>
2020-06-08 13:54:38 +0300

    DI!!!!!!!!!!! ура

---
+++ src/Commands/SerializedCommand.java
@@ -0,0 +1,27 @@
+package Commands;
+
+import java.io.Serializable;
+
+public class SerializedCommand implements Serializable {
+    private Command command;
+    private String login;
+    private String password;
+
+    public SerializedCommand(Command command, String login, String password) {
+        this.command = command;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
